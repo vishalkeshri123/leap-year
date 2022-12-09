62b96bed81fe8bbb0a3632d64c2075b1bae94f05
vishal kumar <118764561+vishalkeshri123@users.noreply.github.com>
2022-12-09 14:34:35 +0530

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,5 +1,5 @@
 y=int(input("enter your year : "))
-if (y % 400 == 0) or (y % 4 == 0 and y % 100 != 0) :
-    print("It is a leap year")
+if (y % 400 == 0) or (y % 4==0 and y % 100 != 0) :
+    print("This is a leap year")
 else :
     print("It is not a leap year")
